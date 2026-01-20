3f3aad99f991e0ed6476584a5ca714f91cd7cf85
Ekuo79 <ekuo3242@gmail.com>
2026-01-20 10:26:24 -0500

    Remove SessionAuthentication from API

--- beetlesgallery/settings.py
+++ beetlesgallery/settings.py
@@ -214,7 +214,6 @@ CSRF_COOKIE_DOMAIN = os.environ.get('SESSION_COOKIE_DOMAIN', None)
 REST_FRAMEWORK = {
     # No pagination for now
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',  # For web users (existing login)
         'rest_framework.authentication.TokenAuthentication',     # For API clients
     ],
     'DEFAULT_PERMISSION_CLASSES': [
