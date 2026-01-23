3b51be77a32377225b16a0d9907f7135149c200d
Layla Schuster <laylaaschuster@ufl.edu>
2026-01-23 17:31:50 -0500

    added jiri's email, changed message to red

--- beetlesgallery/beetles_app/views.py
+++ beetlesgallery/beetles_app/views.py
@@ -542,8 +542,8 @@ def beetle_detail(request, beetle_id):
 def signup(request):
     # --- Security Check: Block non-staff users ---
     if not request.user.is_staff:
-        messages.info(request, "Account creation is restricted. Please email to request an account.")
-        return redirect("login")
+        messages.error(request, "Account creation is restricted. Please email Jiri Hulcr at hulcr@ufl.edu to request an account.")
+        return redirect("beetles_home")
     # ---------------------------------------------
     
     if request.method == "POST":
